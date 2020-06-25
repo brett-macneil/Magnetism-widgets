3a8dcef72d2ce8f26b12ee359f94f918e0f1c7aa
brett-macneil <brett.macneil@dal.ca>
2020-06-25 13:49:39 -0300

    Set up figure
    Two subfigures: M vs T and a plot of the implicit curves and solution intersects.

--- code/Neel Ferrimagnet/neel_iso_nofield_widget.py
+++ code/Neel Ferrimagnet/neel_iso_nofield_widget.py
@@ -12,3 +12,22 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 from scipy.optimize import fsolve
 from scipy.constants import physical_constants as cst
+
+
+### Set up figure
+###______________________________________________________________
+
+# Generate figure
+fig, [ax1, ax2] = plt.subplots(1, 2)
+plt.subplots_adjust(bottom=0.4)
+plt.style.use('dark_background')
+#plt.style.use('lab')
+
+# Label axes
+ax1.set_xlabel(r'$M_{b}$ (kA m$^{-1}$)', fontsize=16)
+ax1.set_ylabel(r'$M_{a}$ (kA m$^{-1}$)', fontsize=16)
+ax1.grid(color='grey', linestyle='dotted', linewidth=1)
+
+ax2.set_xlabel('Temperature (K)', fontsize=16)
+ax2.set_ylabel(r'Magnetization (kA m$^{-1}$)', fontsize=16)
+ax2.grid(color='grey', linestyle='dotted', linewidth=1)
