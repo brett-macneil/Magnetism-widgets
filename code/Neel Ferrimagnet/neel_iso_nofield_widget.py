c362721ce7a10964ad3173cfde4df968369d83d2
brett-macneil <brett.macneil@dal.ca>
2020-06-25 14:00:50 -0300

    Assignment of constants

--- code/Neel Ferrimagnet/neel_iso_nofield_widget.py
+++ code/Neel Ferrimagnet/neel_iso_nofield_widget.py
@@ -31,3 +31,32 @@ ax1.grid(color='grey', linestyle='dotted', linewidth=1)
 ax2.set_xlabel('Temperature (K)', fontsize=16)
 ax2.set_ylabel(r'Magnetization (kA m$^{-1}$)', fontsize=16)
 ax2.grid(color='grey', linestyle='dotted', linewidth=1)
+
+
+### Constants
+###______________________________________________________________
+
+numpoints = 500           # Number of points used in equation solver
+
+# Physical constants
+mu0 = cst['vacuum mag. permeability'][0]      # Permeability of free space
+me = cst['electron mass'][0]                  # Electron mass [kg]
+h = cst['Planck constant'][0]                 # Planck constant [J-s]
+hbar = cst['reduced Planck constant'][0]      # Reduced Planck constant
+e = cst['atomic unit of charge'][0]           # Elementary charge [C]
+muB = cst['Bohr magneton'][0]                 # Bohr Magneton
+g = -cst['electron g factor'][0]              # G-factor
+kB = cst['Boltzmann constant'][0]             # Boltzmann constant [J/K]
+
+#Sublattice parameters
+Na = 8.441e27             # Moments per unit volume in sublattice A
+Nb = 12.66e27             # Moments per unit volume in sublattice B
+Ja = 5/2                  # Total AM quantum number for sublattice A
+Jb = 5/2                  # Total AM quantum number for sublattice B
+ga = g                    # Lande g-factor for sublattice A  
+gb = g                    # Lande g-factor for sublattice B
+
+mua_max = ga*muB*Ja       # Maximum moment on sublattice A
+Ma_max = Na*ga*muB*Ja     # Maximum magnetization of sublattice A
+mub_max = gb* muB*Jb      # Maximum moment on sublattice B
+Mb_max = Nb*gb*muB*Jb     # Maximum magnetization of sublattice B
