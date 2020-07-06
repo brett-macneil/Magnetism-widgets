653412ab45c9eee8e3f30cf462e11f925fa65200
brett-macneil <brett.macneil@dal.ca>
2020-07-06 11:37:55 -0300

    Constants

--- code/Neel Ferrimagnet/fitting_tool.py
+++ code/Neel Ferrimagnet/fitting_tool.py
@@ -12,3 +12,31 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 from scipy.optimize import fsolve, curve_fit
 from scipy.constants import physical_constants as cst
+
+### Constants
+###______________________________________________________________
+
+numpoints = 500           # Number of points used in equation solver
+
+# Physical constants
+mu0 = cst['vacuum mag. permeability'][0]      # Permeability of free space
+me = cst['electron mass'][0]                  # Electron mass [kg]
+h = cst['Planck constant'][0]                 # Planck constant [J-s]
+hbar = cst['reduced Planck constant'][0]      # Reduced Planck constant
+e = cst['atomic unit of charge'][0]           # Elementary charge [C]
+muB = cst['Bohr magneton'][0]                 # Bohr Magneton
+g = -cst['electron g factor'][0]              # G-factor
+kB = cst['Boltzmann constant'][0]             # Boltzmann constant [J/K]
+
+#Sublattice parameters
+Na = 8.441e27             # Moments per unit volume in sublattice A
+Nb = 12.66e27             # Moments per unit volume in sublattice B
+Ja = 5/2                  # Total AM quantum number for sublattice A
+Jb = 5/2                  # Total AM quantum number for sublattice B
+ga = g                    # Lande g-factor for sublattice A  
+gb = g                    # Lande g-factor for sublattice B
+
+mua_max = ga*muB*Ja       # Maximum moment on sublattice A
+Ma_max = Na*ga*muB*Ja     # Maximum magnetization of sublattice A
+mub_max = gb* muB*Jb      # Maximum moment on sublattice B
+Mb_max = Nb*gb*muB*Jb     # Maximum magnetization of sublattice B
